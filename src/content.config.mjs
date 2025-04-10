2d1f36cfa995ff8acbc2f146b25d813324be9e05
Hyunjin <work2drink@gmail.com>
2025-04-10 21:38:09 +0900

    route test

--- src/content.config.mjs
+++ src/content.config.mjs
@@ -1,10 +1,6 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content'
-
-// 2. Import loader(s)
 import { glob } from 'astro/loaders'
 
-// 3. Define your collection(s)
 const reviewsCollection = defineCollection({
   loader: glob({ pattern: '**/*.md', base: 'src/content/reviews' }),
   schema: z.object({
@@ -17,8 +13,19 @@ const reviewsCollection = defineCollection({
     layout: z.string().optional(),
   }),
 })
+const authorCollection = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: 'src/content/authors' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    slug: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    layout: z.string().optional(),
+  }),
+})
 
-// 4. Export a single `collections` object to register you collection(s)
+
 export const collections = {
   'reviews': reviewsCollection,
+  'authors': authorCollection,
 };
