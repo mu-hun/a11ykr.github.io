8cd8ae324c3d3855fcd739500eaf9fb1833faf9b
Hyunjin <work2drink@gmail.com>
2025-05-08 22:07:20 +0900

    feat: add Korean language support and community guidelines page

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+  en: 'English',
+  ko: '한국어',
+};
+
+export const defaultLang = 'ko';
+
+export const ui = {
+  ko: {
+    'nav.home': '홈',
+    'nav.about': '소개',
+		'nav.twitter': '트위터',
+		'nav.github': '깃허브',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+		'nav.github': 'Github',
+  },
+} as const;
